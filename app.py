91224a547685c166f79a41692603f950ca63b8ca
Nicholas Winters <nicholas.d.winters@gmail.com>
2021-04-16 22:21:19 -0500

    try again

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ uri = os.getenv("DATABASE_URL")  # or other relevant config var
 if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = (uri,'postgresql:///jiu_jitsu_source'))
+app.config['SQLALCHEMY_DATABASE_URI'] = (uri,'postgresql:///jiu_jitsu_source')
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
