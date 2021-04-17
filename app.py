0a64d09c8c51c357ecb7b00800d1e8372c435bc0
Nicholas Winters <nicholas.d.winters@gmail.com>
2021-04-16 21:15:10 -0500

    checking if i need to not import my secrets file

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import os
 
 # API key can be obtained from YouTube Data API website, and SECRET KEY is in relation to session; create both of these
 from techniques import techDict
-from secrets import API_KEY, SECRET_KEY
+# from secrets import API_KEY, SECRET_KEY
 from models import db, connect_db, User, Technique, Training_Note
 from forms import UserAddForm, LoginForm, TrainingNoteForm, EditTrainingNoteForm, VideoNoteForm
 
