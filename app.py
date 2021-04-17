dd6f8fbb09ff5a08a74717bed9c1ca10732b1f54
Nicholas Winters <nicholas.d.winters@gmail.com>
2021-04-16 22:37:21 -0500

    messing with api key

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ CURR_USER_KEY = "curr_user"
 
 app = Flask(__name__)
 
-app.config['API_KEY'] = os.environ.get('API_KEY')
+API_KEY = os.environ.get('API_KEY')
 # app.config['API_KEY'] = os.environ.get('API_KEY', API_KEY)
 # app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', SECRET_KEY)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
